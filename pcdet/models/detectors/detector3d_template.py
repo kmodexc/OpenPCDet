f61b8634044e25e0d4bb152d52f5395a447760fe
marius schlueter <themrslue@googlemail.com>
2024-10-30 16:04:50 +0100

    add assert

--- pcdet/models/detectors/detector3d_template.py
+++ pcdet/models/detectors/detector3d_template.py
@@ -361,6 +361,7 @@ class Detector3DTemplate(nn.Module):
         for i in range(len(dece_data)):
             cur_data = dece_data[i]
             tps_fps,pd_scores = cur_data
+            assert tps_fps.shape[0] == pd_scores.shape[0], f"{tps_fps.shape[0]} =/= {pd_scores.shape[0]}"
             bins_ind = (pd_scores.detach() * bins).clamp(0,9).int()
             for i in range(bins):
                 filter_bin = bins_ind == i
