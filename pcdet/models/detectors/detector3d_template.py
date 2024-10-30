a3f9bd42661be583570c8c2d424ad9654fcb4a0c
marius schlueter <themrslue@googlemail.com>
2024-10-30 20:08:13 +0100

    fix

--- pcdet/models/detectors/detector3d_template.py
+++ pcdet/models/detectors/detector3d_template.py
@@ -348,7 +348,7 @@ class Detector3DTemplate(nn.Module):
                     iou3d_rcnn = iou3d_nms_utils.boxes_iou3d_gpu(pd_boxes[:, 0:7], cur_gt[:, 0:7])
                     gt_class = cur_gt[:,-1]
                     gt_mask  = torch.zeros((pd_boxes.shape[0],cur_gt.shape[0]),dtype=torch.bool)
-                    gt_mask[:,gt_class] = True
+                    gt_mask[range(pd_boxes.shape[0]),gt_class] = True
                     dets     = iou3d_rcnn > threshold
                     tps      = dets & gt_mask
                     fps      = dets & torch.logical_not(gt_mask)
