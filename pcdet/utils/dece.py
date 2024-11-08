a6a28a0b2c513c8af06cb1e9e9bc8b794f13efc6
marius schlueter <themrslue@googlemail.com>
2024-11-08 21:10:35 +0100

    final

--- pcdet/utils/dece.py
+++ pcdet/utils/dece.py
@@ -152,8 +152,8 @@ def adaptive_focal_loss(gamma, dece_raw, pd_scores_list):
         neg_gammas = gammas.lt(0)
         pos_gammas = torch.logical_not(neg_gammas)
 
-        loss -= (torch.pow(1-pd_score,          gammas)  * torch.log(pd_score) * pos_gammas.float()).sum()
         loss -= (torch.pow(1+pd_score,torch.abs(gammas)) * torch.log(pd_score) * neg_gammas.float()).sum()
+        loss -= (torch.pow(1-pd_score,          gammas)  * torch.log(pd_score) * pos_gammas.float()).sum()
 
     return loss, gamma
 
