f9ecf9440a2979fbbe073474868ff418763b077b
marius schlueter <themrslue@googlemail.com>
2024-11-14 20:42:06 +0100

    print

--- pcdet/models/model_utils/centernet_utils.py
+++ pcdet/models/model_utils/centernet_utils.py
@@ -187,6 +187,8 @@ def decode_bbox_from_heatmap(heatmap, rot_cos, rot_sin, center, center_z, dim,
     center_z = _transpose_and_gather_feat(center_z, inds).view(batch_size, K, 1)
     dim = _transpose_and_gather_feat(dim, inds).view(batch_size, K, 3)
     full_scores = _transpose_and_gather_feat(heatmap, inds)
+    print("full_scores.shape",full_scores.shape)
+    print("scores.shape",full_scores.shape)
 
     angle = torch.atan2(rot_sin, rot_cos)
     xs = xs.view(batch_size, K, 1) + center[:, :, 0:1]
