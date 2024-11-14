80ec06ea08c17afd32adf77af1bffb307438a9c5
marius schlueter <themrslue@googlemail.com>
2024-11-14 01:20:29 +0100

    try with force float

--- pcdet/utils/dece.py
+++ pcdet/utils/dece.py
@@ -101,7 +101,7 @@ def calc_dece(dece_data, bins=15):
     if total_size == 0:
         return 0, None
     dece = torch.zeros_like(avg_scores)
-    dece += bin_size * (avg_scores - tps/(tps+fps)) / total_size
+    dece += bin_size.float() * (avg_scores - tps.float() / bin_size.float()) / total_size
     dece_item = torch.abs(dece).sum()
     print("returned sum: ", dece_item)
     return dece_item, dece
