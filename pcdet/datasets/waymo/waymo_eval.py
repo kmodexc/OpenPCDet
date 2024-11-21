1cd269daed11166a21392f6546b09602b2d09b63
marius schlueter <themrslue@googlemail.com>
2024-11-21 19:41:07 +0100

    fix

--- pcdet/datasets/waymo/waymo_eval.py
+++ pcdet/datasets/waymo/waymo_eval.py
@@ -42,7 +42,9 @@ class OpenPCDetWaymoDetectionMetricsEstimator(tf.test.TestCase):
             for frame_index, info in enumerate(infos):
                 pb.update()
                 if is_gt:
-                    box_mask = np.array([n in class_names for n in info['name']], dtype=np.bool_)
+                    class_names_set = set(class_names)
+                    box_mask = np.isin(info['name'], list(class_names_set))
+                    # box_mask = np.array([n in class_names for n in info['name']], dtype=np.bool_)
                     if 'num_points_in_gt' in info:
                         zero_difficulty_mask = info['difficulty'] == 0
                         info['difficulty'][(info['num_points_in_gt'] > 5) & zero_difficulty_mask] = 1
