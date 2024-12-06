2ce6af1de1480214a22131b19436332239e299f1
marius schlueter <themrslue@googlemail.com>
2024-12-06 13:25:15 +0100

    fix

--- tools/eval_utils/eval_utils.py
+++ tools/eval_utils/eval_utils.py
@@ -72,16 +72,11 @@ def eval_one_epoch(cfg, args, model, dataloader, epoch_id, logger, dist_test=Fal
             # use ms to measure inference time
             disp_dict['infer_time'] = f'{infer_time_meter.val:.2f}({infer_time_meter.avg:.2f})'
 
-        assert f'pred_all_scores' in pred_dicts[0], f"there is only in annos[0]: {list(annos[0])}"
-
         statistics_info(cfg, ret_dict, metric, disp_dict)
         annos = dataset.generate_prediction_dicts(
             batch_dict, pred_dicts, class_names,
             output_path=final_output_dir if args.save_to_file else None
         )
-
-        assert f'pred_all_scores' in annos[0], f"there is only in annos[0]: {list(annos[0])}"
-
         det_annos += annos
         if cfg.LOCAL_RANK == 0:
             progress_bar.set_postfix(disp_dict)
