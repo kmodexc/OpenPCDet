0649b4ed7e9fc1a79ae6c76a7c891f82330f3c03
marius schlueter <themrslue@googlemail.com>
2024-12-06 13:37:10 +0100

    fix

--- pcdet/datasets/waymo/waymo_eval.py
+++ pcdet/datasets/waymo/waymo_eval.py
@@ -200,7 +200,7 @@ class OpenPCDetWaymoDetectionMetricsEstimator(tf.test.TestCase):
         pd_frameid, pd_boxes3d, pd_type, pd_score, pd_overlap_nlz, _, pd_all_scores = self.generate_waymo_type_results(
             prediction_infos, class_name, is_gt=False
         )
-        gt_frameid, gt_boxes3d, gt_type, gt_score, _, gt_difficulty = self.generate_waymo_type_results(
+        gt_frameid, gt_boxes3d, gt_type, gt_score, gt_overlap_nlz, gt_difficulty = self.generate_waymo_type_results(
             gt_infos, class_name, is_gt=True, fake_gt_infos=fake_gt_infos
         )
         pd_boxes3d, pd_frameid, pd_type, pd_score, pd_overlap_nlz, pd_all_scores = self.mask_by_distance(
