390867e786ef4615b604093df9af4c630c44712f
marius schlueter <themrslue@googlemail.com>
2024-12-29 01:38:19 +0100

    fix

--- pcdet/models/dense_heads/center_head.py
+++ pcdet/models/dense_heads/center_head.py
@@ -103,7 +103,7 @@ class CenterHead(nn.Module):
     def build_losses(self):
         self.add_module('hm_loss_func', loss_utils.FocalLossCenterNet())
         self.add_module('reg_loss_func', loss_utils.RegLossCenterNet())
-        self.add_module('dece_loss_func', dece.FullDECELoss(10))
+        self.add_module('dece_loss_func', dece.FullDECELoss(2))
 
     def assign_target_of_single_head(
             self, num_classes, gt_boxes, feature_map_size, feature_map_stride, num_max_objs=500,
@@ -296,7 +296,7 @@ class CenterHead(nn.Module):
             [x['pred_all_scores' if self.dece_loss_func.use_full_scores else 'pred_scores'] for x in self.forward_ret_dict['box_preds']], 
             self.forward_ret_dict['gt_boxes'])
 
-        loss += 20*dece_loss
+        loss += 2*dece_loss
 
         tb_dict['dece_loss'] = dece_loss
 
